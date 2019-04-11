addcd9440a19f10ade252aca55880dcd24e653cf
akmanon <kashish979@gmail.com>
2019-04-11 19:27:19 +0530

    Structured the app and deleted some file

---
+++ src/Component/Stories/Best.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Best extends Component {
+    render() {
+        return (
+            <div>
+                Best
+            </div>
+        );
+    }
+}
+
+export default Best;
